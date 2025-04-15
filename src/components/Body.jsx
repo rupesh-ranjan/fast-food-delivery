39b9596e71d2b6af0a61d15aec91466835fbf60d
rupesh-ranjan <rupesh.ranjan@outlook.in>
2025-04-15 12:37:55 +0530

    fetching data from api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,43 +1,44 @@
 import { useEffect, useState } from "react";
-import { restaurants } from "../assets/data/restaurants-details.json";
 import { RestaurantCard } from "./RestaurantCard";
 
 export function Body() {
-    const [restaurantsData, setRestaurantsData] = useState(restaurants);
-    useEffect(() => {
-        fetchData();
-    }, []);
+  const [restaurantsData, setRestaurantsData] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    async function fetchData() {
-        const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4444751&lng=78.3858388&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-        );
-        const json = await JSON.parse(data);
-        console.log(json);
-    }
-
-    function handleClick() {
-        setRestaurantsData(
-            restaurantsData.filter((restaurant) => restaurant.avgRating > 4.5)
-        );
-    }
-    return (
-        <div className="body">
-            <div className="filter">
-                <button className="filter-btn" onClick={handleClick}>
-                    Top Rated Restaurant
-                </button>
-            </div>
-            {/* <Search /> */}
-            <div className="search">Search</div>
-            <div className="restaurants-container">
-                {restaurantsData.map((restaurant) => (
-                    <RestaurantCard
-                        key={restaurant.id}
-                        restaurantData={restaurant}
-                    />
-                ))}
-            </div>
-        </div>
+  async function fetchData() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4444751&lng=78.3858388&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
+    const json = await data.json();
+    setRestaurantsData(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  }
+
+  function handleClick() {
+    setRestaurantsData(
+      restaurantsData.filter((restaurant) => restaurant.info.avgRating > 4.5)
+    );
+  }
+  return (
+    <div className="body">
+      <div className="filter">
+        <button className="filter-btn" onClick={handleClick}>
+          Top Rated Restaurant
+        </button>
+      </div>
+      {/* <Search /> */}
+      <div className="search">Search</div>
+      <div className="restaurants-container">
+        {restaurantsData.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant.info.id}
+            restaurantData={restaurant.info}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,24 +1,18 @@
 export function RestaurantCard({ restaurantData }) {
-    const {
-        cloudinaryImageId,
-        name,
-        cuisines,
-        avgRating,
-        costForTwo,
-        deliveryTime,
-    } = restaurantData;
-    return (
-        <div className="restaurant-card">
-            <img
-                src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
-                alt="restaurant-card-logo"
-                className="restaurant-logo"
-            />
-            <h3>{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
-            <h4>{avgRating} Stars</h4>
-            <h4>{costForTwo}</h4>
-            <h4>{deliveryTime}</h4>
-        </div>
-    );
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
+    restaurantData;
+  return (
+    <div className="restaurant-card">
+      <img
+        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+        alt="restaurant-card-logo"
+        className="restaurant-logo"
+      />
+      <h3>{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} Stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla?.deliveryTime} mins</h4>
+    </div>
+  );
 }
