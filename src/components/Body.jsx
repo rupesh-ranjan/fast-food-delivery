8cabdec916ca79e2a4ffddee059dd3e8d7287074
rupesh-ranjan <rupesh.ranjan@outlook.in>
2025-04-17 04:46:51 +0530

    Shimmer UI added, Search added for restaurants

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
 import { RestaurantCard } from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 export function Body() {
-  const [restaurantsData, setRestaurantsData] = useState([]);
+  const [allRestaurants, setAllRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [showTopRated, setShowTopRated] = useState(false);
+  const [searchText, setSearchText] = useState("");
+
+  console.log("rendering body");
   useEffect(() => {
     fetchData();
   }, []);
@@ -12,32 +18,67 @@ export function Body() {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4444751&lng=78.3858388&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    setRestaurantsData(
-      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
-    );
+    const restaurants =
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    setAllRestaurants(restaurants);
+    setFilteredRestaurants(restaurants);
   }
 
-  function handleClick() {
-    setRestaurantsData(
-      restaurantsData.filter((restaurant) => restaurant.info.avgRating > 4.5)
-    );
+  function toggleTopRated() {
+    if (showTopRated) {
+      setFilteredRestaurants(allRestaurants);
+    } else {
+      setFilteredRestaurants(
+        filteredRestaurants.filter(
+          (restaurant) => restaurant.info.avgRating >= 4.4
+        )
+      );
+    }
+    setShowTopRated(!showTopRated);
   }
+
+  if (allRestaurants.length === 0) return <Shimmer />;
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn" onClick={handleClick}>
-          Top Rated Restaurant
+        <div className="search">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="search-box"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              const restaurants = allRestaurants.filter((restaurant) =>
+                restaurant.info.name
+                  .toLowerCase()
+                  .includes(searchText.trim().toLowerCase())
+              );
+              setFilteredRestaurants(restaurants);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button className="filter-btn" onClick={toggleTopRated}>
+          {showTopRated ? "Show All Restaurants" : "Show Top Rated Restaurants"}
         </button>
       </div>
-      {/* <Search /> */}
-      <div className="search">Search</div>
       <div className="restaurants-container">
-        {restaurantsData.map((restaurant) => (
-          <RestaurantCard
-            key={restaurant.info.id}
-            restaurantData={restaurant.info}
-          />
-        ))}
+        {filteredRestaurants.length === 0 ? (
+          <p>No restaurants found.</p>
+        ) : (
+          filteredRestaurants.map((restaurant) => (
+            <RestaurantCard
+              key={restaurant.info.id}
+              restaurantData={restaurant.info}
+            />
+          ))
+        )}
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
+
 export function Header() {
-    return (
-        <div className="header">
-            <div className="logo-container">
-                <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk8cDvKIAenhsySbKd8VM7JbxL1lViGsbDJw&s"
-                    alt="logo"
-                    className="logo"
-                />
-            </div>
-            <div className="nav-items">
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
-                </ul>
-            </div>
-        </div>
-    );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk8cDvKIAenhsySbKd8VM7JbxL1lViGsbDJw&s"
+          alt="logo"
+          className="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <li>
+            <button
+              className="login-btn"
+              onClick={() => setIsLoggedIn(!isLoggedIn)}
+            >
+              {isLoggedIn ? "Logout" : "Login"}
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
