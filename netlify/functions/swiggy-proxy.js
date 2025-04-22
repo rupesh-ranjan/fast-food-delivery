d42142a33d67217749a4166092a431d42e2e7ea1
rupesh-ranjan <rupesh.ranjan@outlook.in>
2025-04-22 05:54:52 +0530

    fixing cors issue on netlify

---
+++ netlify/functions/swiggy-proxy.js
@@ -0,0 +1,31 @@
+// netlify/functions/swiggy-proxy.js
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  const { lat = "17.4444751", lng = "78.3858388" } =
+    event.queryStringParameters;
+
+  const swiggyURL = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
+
+  try {
+    const response = await axios.get(swiggyURL, {
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+      },
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch from Swiggy" }),
+    };
+  }
+};
