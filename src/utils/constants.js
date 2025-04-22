d4ecf8d4931835d330169eba4300fa47252870d7
rupesh-ranjan <rupesh.ranjan@outlook.in>
2025-04-22 16:42:58 +0530

    fix for cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4444751&lng=78.3858388&restaurantId=";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4444751&lng=78.3858388&restaurantId=";
 
 export const RESTAURANT_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4444751&lng=78.3858388&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4444751&lng=78.3858388&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const GITHUB_API = "https://api.github.com/users/rupesh-ranjan";
 
--- src/utils/useRestaurants.js
+++ src/utils/useRestaurants.js
@@ -4,34 +4,20 @@ import { RESTAURANT_API } from "./constants";
 export const useRestaurants = () => {
   const [allRestaurants, setAllRestaurants] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
-  const lat = 17.4444751;
-  const lng = 78.3858388;
+
   useEffect(() => {
-    // fetchData();
-    fetch(`/.netlify/functions/swiggy-proxy?lat=${lat}&lng=${lng}`)
-      .then((res) => {
-        console.log(res);
-        return res.json();
-      })
-      .then((data) => {
-        console.log("Swiggy data:", data);
-        const restaurants =
-          data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
-            ?.restaurants;
-        setAllRestaurants(restaurants);
-        setFilteredRestaurants(restaurants);
-      });
+    fetchData();
   }, []);
 
-  //   async function fetchData() {
-  //     const data = await fetch(RESTAURANT_API);
-  //     const json = await data.json();
-  //     const restaurants =
-  //       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
-  //         ?.restaurants;
-  //     setAllRestaurants(restaurants);
-  //     setFilteredRestaurants(restaurants);
-  //   }
+  async function fetchData() {
+    const data = await fetch(RESTAURANT_API);
+    const json = await data.json();
+    const restaurants =
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants;
+    setAllRestaurants(restaurants);
+    setFilteredRestaurants(restaurants);
+  }
 
   return { allRestaurants, filteredRestaurants, setFilteredRestaurants };
 };
